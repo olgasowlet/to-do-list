f69b76cf05bb0d124e6e18b266a12cbda477f85d
Olga Sowinska <olga.sowinska00@gmail.com>
2020-07-21 15:40:26 +0200

    new tasks can be added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 {
-    const formElement = document.querySelector(".js-form");
-
-    formElement.addEventListener('click', (event) => {
-        event.preventDefault(formElement);
-    });
-
     const tasks = [
         {
             content: "Zjeść obiad",
@@ -30,8 +24,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+
+        tasks.push({
+            content: newTaskContent,
+            done: false,
+        });
+
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-input").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
+
     const init = () => {
         render();
+
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener('submit', onFormSubmit);
     };
 
     init();
