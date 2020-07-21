dbb5476aecca94647ce5a6f2a2a7acff72308b25
Olga Sowinska <olga.sowinska00@gmail.com>
2020-07-21 14:51:46 +0200

    changes with media query, created script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+{
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener('click', (event) => {
+        event.preventDefault(formElement);
+    });
+
+    const tasks = [
+        {
+            content: "Zjeść obiad",
+            done: false,
+        },
+        {
+            content: "Zjeść czekoladę",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${tasks.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
